ff26bd8c8627186a0592f650e2d5fde20797cbe7
denisse1690 <102622269+denisse1690@users.noreply.github.com>
2022-10-04 17:12:36 -0500

    Create index.php

---
+++ php/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Hola Mundo</title>
+</head>
+<body>
+ <h1><?php echo "Hola mundo"; ?></h1>	
+ <?php 
+ /*
+$mi_variable = 1234.12;
+$texto = '1234';
+$variable = "numero:12";
+$boleana = TRUE;
+$falfo= FALSE;
+
+echo "El valor de la variable texto es: {$texto} ".$texto;
+
+define('CONSTANTE', "Hola soy una constante");
+define('PI', 3.14);
+define('CONSTANTE', 3.14);
+
+echo "El valor de PI es: ".PI;
+
+//Hola esto es un comentario
+#Hola este es otro comentario
+#comentario echo "Esto es un mensaje"; #comentario
+*/
+/*
+1
+2
+3
+*/
+
+  ?>
+</body>
+</html>
